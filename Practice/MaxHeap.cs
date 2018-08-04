88d5103cfde471bd5a4f67e5c67624ccad8d8f7c
Tim <tmbond@gmail.com>
2018-08-04 16:22:40 -0400

    Initial commit. Also including tests for a MaxHeap implementation.

---
+++ Practice/MaxHeap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice
+{
+    public class MaxHeap
+    {
+        public int GetMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
