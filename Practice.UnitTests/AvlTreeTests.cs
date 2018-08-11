f4b431dec65f268c832ae5c38ad27fc2f9e014d6
Tim <tmbond@gmail.com>
2018-08-11 14:14:17 -0400

    Adding class/tests for AVL tree.

---
+++ Practice.UnitTests/AvlTreeTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+using System.Linq;
+
+namespace Practice.UnitTests
+{
+    [TestFixtureSource("TestCases")]
+    public class AvlTreeTests
+    {
+        // TODO: Add some other test cases, probably rewrite as
+        // a generator with a random seed so it stays the same
+        // Pre-randomized list of integers from 1 through 50
+        private readonly static List<int> Numbers = new List<int>
+        {
+            17, 8, 48, 46, 30, 31, 18, 33, 20, 22,
+            34, 3, 50, 42, 27, 38, 9, 19, 35, 4,
+            11, 37, 29, 14, 15, 24, 44, 7, 26, 1,
+            43, 32, 13, 21, 16, 12, 5, 41, 6, 45,
+            49, 47, 10, 2, 36, 23, 40, 28, 25, 39
+        };
+
+        static object[] TestCases = BuildTestCases();
+
+        private AvlTree tree;
+
+        private List<int> numbers;
+
+        static object[] BuildTestCases()
+        {
+            var testCases = new List<object[]>();
+
+            for (var i = 1; i < Numbers.Count; i++)
+            {
+                testCases.Add(new object[] { Numbers.Take(i).ToList() });
+            }
+
+            return testCases.ToArray<object>();
+        }
+
+        public AvlTreeTests(List<int> numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            tree = new AvlTree();
+
+            foreach (var number in numbers)
+            {
+                tree.Insert(number);
+            }
+        }
+
+        [Test]
+        public void TraversalIsNumbersInOrder()
+        {
+            var results = new List<int>();
+
+            tree.InOrderTraverse(n => results.Add(n.Value));
+
+            Assert.AreEqual(numbers.Count, results.Count);
+
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                Assert.Greater(results[i], results[i - 1]);
+            }
+        }
+        
+        [Test]
+        public void AllSubtreesAreWithinOneHeight()
+        {
+            tree.InOrderTraverse(n =>
+            {
+                if (Math.Abs(GetHeight(n.LeftChild) - GetHeight(n.RightChild)) > 1)
+                {
+                    Assert.Fail("Tree not balanced at node {0}.", n);
+                }
+            });
+        }
+
+        private int GetHeight(AvlTree.Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            
+            return 1 + Math.Max(GetHeight(node.LeftChild), GetHeight(node.RightChild));
+        }
+        
+        private List<List<AvlTree.Node>> GetPaths(AvlTree tree)
+        {
+            var paths = new List<List<AvlTree.Node>>();
+            var ancestorsAndSelf = new Stack<AvlTree.Node>();
+
+            if (tree.Root == null)
+            {
+                return paths;
+            }
+
+            ancestorsAndSelf.Push(tree.Root);
+            BuildPaths(tree.Root, ancestorsAndSelf, paths);
+
+            return paths;
+        }
+
+        private void BuildPaths(AvlTree.Node node, Stack<AvlTree.Node> ancestorsAndSelf, List<List<AvlTree.Node>> results)
+        {
+            if (node.LeftChild == null || node.RightChild == null)
+            {
+                results.Add(ancestorsAndSelf.ToList());
+            }
+
+            var children = new List<AvlTree.Node> { node.LeftChild, node.RightChild };
+            children.RemoveAll(item => item == null);
+
+            foreach (var child in children)
+            {
+                ancestorsAndSelf.Push(child);
+                BuildPaths(child, ancestorsAndSelf, results);
+                ancestorsAndSelf.Pop();
+            }
+        }
+    }
+}
---
+++ Practice/AvlTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class AvlTree
+    {
+        public Node Root { get; private set; }
+
+        public void Insert(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InOrderTraverse(Action<Node> traversalAction)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public class Node
+        {
+            public int Value { get; private set; }
+
+            public Node LeftChild { get; set; }
+
+            public Node RightChild { get; set; }
+
+            public int Height { get; }
+        }
+    }
+}
