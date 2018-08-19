87d40f2630b490f38df77f9e5c75c6366de5f24f
Tim <tmbond@gmail.com>
2018-08-19 15:23:53 -0400

    Added sorting algorithm test.

---
+++ Practice.UnitTests/SorterTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Practice.UnitTests
+{
+    [TestFixture]
+    public class SorterTests
+    {
+        // Pre-randomized list of integers from 1 through 50
+        private readonly static List<int> Numbers = new List<int>
+        {
+            17, 8, 48, 46, 30, 31, 18, 33, 20, 22,
+            34, 3, 50, 42, 27, 38, 9, 19, 35, 4,
+            11, 37, 29, 14, 15, 24, 44, 7, 26, 1,
+            43, 32, 13, 21, 16, 12, 5, 41, 6, 45,
+            49, 47, 10, 2, 36, 23, 40, 28, 25, 39
+        };
+
+        public void ReturnsNumbersInOrder()
+        {
+            var sortedNumbers = Sorter.Sort(Numbers);
+
+            Assert.AreEqual(50, sortedNumbers.Count);
+
+            for (var i = 0; i < sortedNumbers.Count; i++)
+            {
+                Assert.AreEqual(i + 1, sortedNumbers[i]);
+            }
+        }
+    }
+}
---
+++ Practice/Sorter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    public static class Sorter
+    {
+        /*
+         * Some well-known sorting algorithms:
+         * 
+         * Heapsort - build a heap out of the data. Repeatedly remove the max item and put it at the end
+         *            (reducing the size of the 'heap' part of the array).
+         *            n log n worst-case performance, constant memory, unstable.
+         * 
+         * Merge sort - divide-and-conquer on sub-arrays. Break array into arrays of size 1 and then slowly merge them.
+         *              n log n worst-case time, n memory, stable.
+         * 
+         * Bubble sort - Loop through the list and swap adjacent items if they are out of order.
+         *               n^2 time. Very easy to code. Stable.
+         * 
+         */
+
+        public static List<int> Sort(IReadOnlyList<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
