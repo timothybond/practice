3ab090278cb8a523950a20b74b69ee885e683a2a
kiacolbert <colber16@gmail.com>
2018-09-16 21:50:08 -0400

    Added tail.

---
+++ Practice/DoublyLinkedNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice
+{
+    public class DoublyLinkedNode
+    {
+        public DoublyLinkedNode Previous;
+
+        public DoublyLinkedNode Next;
+
+        public int Data;
+
+        public DoublyLinkedNode(){}
+
+        public DoublyLinkedNode(int data)
+        {
+            this.Data = data;
+        }
+    }
+}
--- Practice/LinkedList.cs
+++ Practice/LinkedList.cs
@@ -4,11 +4,53 @@ namespace Practice
 {
     public class LinkedList
     {
-        public int Count { get; }
+        private DoublyLinkedNode head;
+        private DoublyLinkedNode tail;
+
+        public int Count
+        {
+            get
+            {
+                var current = this.head;
+                var count = 0;
+                while (current != null)
+                {
+                    count++;
+                    current = current.Next;
+                }
+                return count;
+            }
+        }
 
         public void Add(int value)
         {
-            throw new NotImplementedException();
+            var node = new DoublyLinkedNode(value);
+
+            if(head == null)
+            {
+                head = node;
+                tail = head;
+                return;
+            }
+            if (tail != null)
+            {
+                node.Previous = tail;
+                tail.Next = node;
+                tail = node;
+            }
+            //var current = this.head;
+
+            //while(current != null)
+            //{
+            //    if(current.Next == null)
+            //    {
+            //        current.Next = node;
+            //        node.Previous = current;
+            //        return;
+            //    }
+            //    current = current.Next;
+
+            //}
         }
 
         public void Insert(int value, int index)
@@ -23,7 +65,15 @@ namespace Practice
 
         public int[] GetEntries()
         {
-            throw new NotImplementedException();
+            var current = this.head;
+            var entries = new int[Count];
+
+            for (int i = 0; i < Count; i++)
+            {
+                entries[i] = current.Data;
+                current = current.Next;
+            }
+            return entries;
         }
     }
 }
