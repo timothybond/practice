9a1d92a70a62c8d28e1d397e71f162508bea22d5
Kia <kia@thisComputer.home>
2018-10-08 09:44:39 -0400

    Starting over.

--- Practice/LinkedList.cs
+++ Practice/LinkedList.cs
@@ -2,129 +2,24 @@
 
 namespace Practice
 {
-    public class LinkedList
-    {
-		public class DoublyLinkedNode
-        {
-            public DoublyLinkedNode Previous;
-
-            public DoublyLinkedNode Next;
-
-            public int Data;
-         
-            public DoublyLinkedNode(int data)
-            {
-                this.Data = data;
-            }
-        }
-        private DoublyLinkedNode head;
-        private DoublyLinkedNode tail;
-
-        public int Count
-        {
-            get
-            {
-				var count = 0;
-
-				for (var current = this.head; current != null; current = current.Next)
-                {
-                    count++;
-                }
-                return count;
-            }
-        }
-
-        public void Add(int value)
-        {
-            var node = new DoublyLinkedNode(value);
-
-            if(head == null)
-            {
-                head = node;
-                tail = head;
-                return;
-            }
-            else
-            {
-                node.Previous = tail;
-                tail.Next = node;
-                tail = node;
-            }
-     
-        }
-
-        public void Insert(int value, int index)
-        {
-            var node = new DoublyLinkedNode(value);
-           
-
-            if(index == 0)
-			{
-                node.Next = this.head;
-                this.head = node;
-                return;
-            }
-            if(index == this.Count)
-            {
-                Add(value);
-                return;
-            }
-
-			var current = head;
-
-            for (int i = 0; i < index; i++)
-            {
-                current = current.Next;
-            }
-			var before = current.Previous;
-            before.Next = node;
-            node.Previous = before;
-            node.Next = current;
-            current.Previous = node;
-           
-        }
-
-		public void RemoveFirst(int value)
+   public class LinkedList
+	{
+		private class Node
 		{
-			if (this.head.Data == value)
+			public int count;
+
+			public Node Next;
+
+			public int Data;
+
+            public Node(int data)
 			{
-				var next = this.head.Next;
-
-				if (this.head.Next != null)
-				{
-					next.Previous = null;
-				}
-				this.head = next;
-
-				return;
+				this.Data = data;
+				this.Next = null;
 			}
-			for (var current = this.head; current != null; current = current.Next)
-            {
-				if (current.Data == value)
-				{
-					current.Previous.Next = current.Next;
-
-                    if(current.Previous.Next != null)
-					{
-						current.Previous.Next.Previous = current.Previous;
-					}
-					return;
-				}
-			}
-
 		}
+		private Node head;
+		public int count;
 
-        public int[] GetEntries()
-        {
-			var index = 0;
-            var entries = new int[Count];
-
-			for (var current = this.head; current != null; current = current.Next)
-            {
-                entries[index] = current.Data;
-				index++;
-            }
-            return entries;
-        }
-    }
+	}
 }
