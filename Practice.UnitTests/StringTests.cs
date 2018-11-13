5ddec0699de328d677e577845d0f5a8e5f0d80ef
kiacolbert <colber16@gmail.com>
2018-11-13 09:25:18 -0500

    argh length.

---
+++ Practice.UnitTests/StringTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+namespace Practice.UnitTests
+{
+    [TestFixture()]
+    public class StringTests
+    {
+        [TestCase(2, 'r')]
+        public void Insert(int num, char c)
+        {
+            var sb = new StringBuilder();
+            sb.Append('c');
+            sb.Append('h');
+            sb.Append('o');
+            sb.Insert(num, c);
+            Assert.AreEqual('r', sb[2]);
+        }
+        [TestCase(2, 'e')]
+        public void InsertOverCapacity(int num, char c)
+        {
+            var sb = new StringBuilder();
+            sb.Append('c');
+            sb.Append('h');
+            sb.Append('o');
+            sb.Append('r');
+            sb.Append('e');
+            sb.Append('s');
+            sb.Append('a');
+            sb.Append('b');
+            sb.Append('o');
+            sb.Append('r');
+            sb.Insert(num, c);
+            Assert.AreEqual('e', sb[2]);
+            Assert.AreEqual('r', sb[10]);
+        }
+        [TestCase('j')]
+        public void AppendToEmpty(char c)
+        {
+            var sb = new StringBuilder();
+            sb.Append(c);
+            Assert.AreEqual(c, sb[0]);
+        }
+        [TestCase('j')]
+        public void Append(char c)
+        {
+            var sb = new StringBuilder();
+            sb.Append('o');
+            sb.Append(c);
+            Assert.AreEqual(c, sb[1]);
+        }
+        [Test]
+        public void ToStringReturnsString()
+        {
+            var sb = new StringBuilder();
+             sb.Append('c');
+            sb.Append('h');
+            sb.Append('o');
+            sb.Append('r');
+            sb.Append('e');
+            var s = sb.ToString();
+            Assert.AreEqual("chore", s);
+        }
+    }
+}
---
+++ Practice/StringBuilder.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+namespace Practice
+{
+    public class StringBuilder
+    {
+        private static int capacity = 10;
+        private char[] arr = new char[capacity];
+        
+        public int Capacity
+        {
+            get=> capacity;
+            set => capacity = value;
+        }
+        public char this [int index]
+        {
+            get => arr[index];
+            set => arr[index] = value;
+        }
+        public int Length 
+        {
+            get;
+            private set;
+            
+        }
+        public StringBuilder()
+        {
+            
+        }
+        public void Append(char c)
+        {
+            CheckCapacity();
+            arr[this.Length] = c;
+
+            this.Length++;
+        }
+
+        private void CheckCapacity()
+        {
+            if (this.Length > this.Capacity)
+            {
+                this.Capacity *= 2;
+                Array.Resize(ref arr, this.Capacity);
+
+            }
+        }
+        private void CheckCapacity(int index)
+        {
+            if (index >= this.Capacity)
+            {
+                this.Capacity *= 2;
+                Array.Resize(ref arr, this.Capacity);
+
+            }
+        }
+
+        public void Insert(int n, char c)
+        {
+            
+            char temp;
+            char replacement = c;
+            for (int j = n; j <= this.Length; j++)
+            {
+                CheckCapacity(j);
+                temp = arr[j];
+                arr[j] = replacement;
+                replacement = temp;
+            }
+            this.Length++;
+        }
+        public void Remove(int startIndex, int length)
+        {
+            this.Length--;
+        }
+        public override string ToString()
+        {
+            int length = arr.Length;
+            return new string(arr, 0, length);
+        }
+    }
+}
