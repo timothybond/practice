741f0ffeff3b364b9ca4aa3e7215e6f1df1ee568
tim <9168829+timothybond@users.noreply.github.co>
2022-09-03 14:04:19 -0500

    Adding ability to print out a tree as a string for debugging (probably could use refactoring). Split up some tree tests.

--- Practice.UnitTests/AvlTreeTests.cs
+++ Practice.UnitTests/AvlTreeTests.cs
@@ -6,196 +6,248 @@ using System.Linq;
 
 namespace Practice.UnitTests
 {
-    [TestFixtureSource("TestCases")]
+    
     public class AvlTreeTests
     {
-        // TODO: Add some other test cases, probably rewrite as
-        // a generator with a random seed so it stays the same
-        // Pre-randomized list of integers from 1 through 50
-        private readonly static List<int> Numbers = new()
+        [TestFixtureSource("TestCases")]
+        public class MultipleAvlTreeTests
         {
-            17, 8, 48, 46, 30, 31, 18, 33, 20, 22,
-            34, 3, 50, 42, 27, 38, 9, 19, 35, 4,
-            11, 37, 29, 14, 15, 24, 44, 7, 26, 1,
-            43, 32, 13, 21, 16, 12, 5, 41, 6, 45,
-            49, 47, 10, 2, 36, 23, 40, 28, 25, 39
-        };
+            // TODO: Add some other test cases, probably rewrite as
+            // a generator with a random seed so it stays the same
+            // Pre-randomized list of integers from 1 through 50
+            private readonly static List<int> Numbers = new()
+            {
+                17,
+                8,
+                48,
+                46,
+                30,
+                31,
+                18,
+                33,
+                20,
+                22,
+                34,
+                3,
+                50,
+                42,
+                27,
+                38,
+                9,
+                19,
+                35,
+                4,
+                11,
+                37,
+                29,
+                14,
+                15,
+                24,
+                44,
+                7,
+                26,
+                1,
+                43,
+                32,
+                13,
+                21,
+                16,
+                12,
+                5,
+                41,
+                6,
+                45,
+                49,
+                47,
+                10,
+                2,
+                36,
+                23,
+                40,
+                28,
+                25,
+                39
+            };
 
 #pragma warning disable IDE0052 // Remove unread private members
-        static object[] TestCases = BuildTestCases();
+            static object[] TestCases = BuildTestCases();
 #pragma warning restore IDE0052 // Remove unread private members
 
-        // Note temporary values are for nullability
-        private AvlTree tree = new();
+            // Note temporary values are for nullability
+            private AvlTree tree = new();
 
-        private List<int> numbers = new();
+            private List<int> numbers = new();
 
-        private List<int> numbersToDelete = new();
+            private List<int> numbersToDelete = new();
 
-        static object[] BuildTestCases()
-        {
-            var testCases = new List<object[]>();
-
-            for (var i = 1; i < Numbers.Count; i++)
+            static object[] BuildTestCases()
             {
-                for (var j = 0; j <= i; j++)
-                { 
-                    testCases.Add(new object[] { Numbers.Take(i).ToList(), Numbers.Take(j).ToList() });
-                }
-            }
+                var testCases = new List<object[]>();
 
-            return testCases.ToArray<object>();
-        }
-
-        public AvlTreeTests(List<int> numbers, List<int> numbersToDelete)
-        {
-            this.numbers = numbers;
-            this.numbersToDelete = numbersToDelete;
-        }
-
-        [SetUp]
-        public void Setup()
-        {
-            tree = new AvlTree();
-
-            foreach (var number in numbers)
-            {
-                tree.Insert(number);
-            }
-
-            foreach (var number in numbersToDelete)
-            {
-                tree.Delete(number);
-            }
-        }
-
-        [Test]
-        public void TraversalIsNumbersInOrder()
-        {
-            var results = new List<int>();
-
-            tree.InOrderTraverse(n => results.Add(n.Value));
-
-            Assert.AreEqual(numbers.Count - numbersToDelete.Count, results.Count);
-
-            for (var i = 1; i < numbers.Count - numbersToDelete.Count; i++)
-            {
-                Assert.Greater(results[i], results[i - 1]);
-            }
-        }
-
-        [Test]
-        public void AllSubtreesAreWithinOneHeight()
-        {
-            tree.InOrderTraverse(n =>
-            {
-                if (Math.Abs(GetHeight(n.LeftChild) - GetHeight(n.RightChild)) > 1)
+                for (var i = 1; i < Numbers.Count; i++)
                 {
-                    Assert.Fail("Tree not balanced at node {0}.", n);
+                    for (var j = 0; j <= i; j++)
+                    {
+                        testCases.Add(new object[] { Numbers.Take(i).ToList(), Numbers.Take(j).ToList() });
+                    }
                 }
-            });
-        }
 
-        [Test]
-        public void OnInsert_RotateRight()
-        {
-            tree = new AvlTree();
-            tree.Insert(2);
-            tree.Insert(1);
-            tree.Insert(0);
-
-            var results = new List<int>();
-            tree.InOrderTraverse(n => results.Add(n.Value));
-
-            Assert.AreEqual(3, results.Count);
-
-            for (var i = 0; i < 2; i++)
-            {
-                Assert.AreEqual(i, results[i]);
+                return testCases.ToArray<object>();
             }
 
-            Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
-        }
-
-        [Test]
-        public void OnInsert_RotateLeft()
-        {
-            tree = new AvlTree();
-            tree.Insert(0);
-            tree.Insert(1);
-            tree.Insert(2);
-
-            var results = new List<int>();
-            tree.InOrderTraverse(n => results.Add(n.Value));
-
-            Assert.AreEqual(3, results.Count);
-
-            for (var i = 0; i < 2; i++)
+            public MultipleAvlTreeTests(List<int> numbers, List<int> numbersToDelete)
             {
-                Assert.AreEqual(i, results[i]);
+                this.numbers = numbers;
+                this.numbersToDelete = numbersToDelete;
             }
 
-            Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
-        }
-
-        [Test]
-        public void OnDelete_RotateRight()
-        {
-            tree = new AvlTree();
-            tree.Insert(2);
-            tree.Insert(1);
-            tree.Insert(3);
-            tree.Insert(0);
-
-            tree.Delete(3);
-
-            var results = new List<int>();
-            tree.InOrderTraverse(n => results.Add(n.Value));
-
-            Assert.AreEqual(3, results.Count);
-
-            for (var i = 0; i < 2; i++)
+            [SetUp]
+            public void Setup()
             {
-                Assert.AreEqual(i, results[i]);
+                tree = new AvlTree();
+
+                foreach (var number in numbers)
+                {
+                    tree.Insert(number);
+                }
+
+                foreach (var number in numbersToDelete)
+                {
+                    tree.Delete(number);
+                }
             }
 
-            Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
-        }
-
-        [Test]
-        public void OnDelete_RotateLeft()
-        {
-            tree = new AvlTree();
-            tree.Insert(0);
-            tree.Insert(1);
-            tree.Insert(-1);
-            tree.Insert(2);
-
-            tree.Delete(-1);
-
-            var results = new List<int>();
-            tree.InOrderTraverse(n => results.Add(n.Value));
-
-            Assert.AreEqual(3, results.Count);
-
-            for (var i = 0; i < 2; i++)
+            [Test]
+            public void TraversalIsNumbersInOrder()
             {
-                Assert.AreEqual(i, results[i]);
+                var results = new List<int>();
+
+                tree.InOrderTraverse(n => results.Add(n.Value));
+
+                Assert.AreEqual(numbers.Count - numbersToDelete.Count, results.Count);
+
+                for (var i = 1; i < numbers.Count - numbersToDelete.Count; i++)
+                {
+                    Assert.Greater(results[i], results[i - 1]);
+                }
             }
 
-            Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
+            [Test]
+            public void AllSubtreesAreWithinOneHeight()
+            {
+                tree.InOrderTraverse(n =>
+                {
+                    if (Math.Abs(GetHeight(n.LeftChild) - GetHeight(n.RightChild)) > 1)
+                    {
+                        Assert.Fail("Tree not balanced at node {0}.", n);
+                    }
+                });
+            }
+
+            private int GetHeight(AvlTree.Node? node)
+            {
+                if (node == null)
+                {
+                    return 0;
+                }
+
+                return 1 + Math.Max(GetHeight(node.LeftChild), GetHeight(node.RightChild));
+            }
         }
 
-        private int GetHeight(AvlTree.Node? node)
+        public class SingleAvlTreeTests
         {
-            if (node == null)
+            [Test]
+            public void OnInsert_RotateRight()
             {
-                return 0;
+                var tree = new AvlTree();
+                tree.Insert(2);
+                tree.Insert(1);
+                tree.Insert(0);
+
+                var results = new List<int>();
+                tree.InOrderTraverse(n => results.Add(n.Value));
+
+                Assert.AreEqual(3, results.Count);
+
+                for (var i = 0; i < 2; i++)
+                {
+                    Assert.AreEqual(i, results[i]);
+                }
+
+                Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
             }
-            
-            return 1 + Math.Max(GetHeight(node.LeftChild), GetHeight(node.RightChild));
+
+            [Test]
+            public void OnInsert_RotateLeft()
+            {
+                var tree = new AvlTree();
+                tree.Insert(0);
+                tree.Insert(1);
+                tree.Insert(2);
+
+                var results = new List<int>();
+                tree.InOrderTraverse(n => results.Add(n.Value));
+
+                Assert.AreEqual(3, results.Count);
+
+                for (var i = 0; i < 2; i++)
+                {
+                    Assert.AreEqual(i, results[i]);
+                }
+
+                Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
+            }
+
+            [Test]
+            public void OnDelete_RotateRight()
+            {
+                var tree = new AvlTree();
+                tree.Insert(2);
+                tree.Insert(1);
+                tree.Insert(3);
+                tree.Insert(0);
+
+                tree.Delete(3);
+
+                var results = new List<int>();
+                tree.InOrderTraverse(n => results.Add(n.Value));
+
+                Assert.AreEqual(3, results.Count);
+
+                for (var i = 0; i < 2; i++)
+                {
+                    Assert.AreEqual(i, results[i]);
+                }
+
+                Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
+            }
+
+            [Test]
+            public void OnDelete_RotateLeft()
+            {
+                var tree  = new AvlTree();
+                tree.Insert(0);
+                tree.Insert(1);
+                tree.Insert(-1);
+                tree.Insert(2);
+
+                tree.Delete(-1);
+
+                var results = new List<int>();
+                tree.InOrderTraverse(n => results.Add(n.Value));
+
+                Assert.AreEqual(3, results.Count);
+
+                for (var i = 0; i < 2; i++)
+                {
+                    Assert.AreEqual(i, results[i]);
+                }
+
+                Assert.AreEqual(2, tree.Root!.Height, "Tree is not balanced to 2 layers.");
+            }
         }
-        
+
         private List<List<AvlTree.Node>> GetPaths(AvlTree tree)
         {
             var paths = new List<List<AvlTree.Node>>();
--- Practice/AvlTree.cs
+++ Practice/AvlTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Practice
 {
@@ -20,14 +21,145 @@ namespace Practice
         {
             throw new NotImplementedException();
         }
-        
+
+        public override string ToString()
+        {
+            if (Root == null)
+            {
+                return "[empty]";
+            }
+            
+            if (Root.Height == 1)
+            {
+                return Root.Value.ToString();
+            }
+
+            // The bottom-most row in a tree of height n has 2^(n-1) nodes.
+
+            // Assuming two-digit numbers at a max, each node takes up
+            // 2 characters with an additional space between (3 total).
+
+            // So the overall width of the structure is 3 * 2^(n-1)
+
+            var width = 3 * (int)Math.Pow(2, Root.Height - 1);
+
+            var totalHeight = Root.Height * 2 - 1;
+
+            var output = new char[width, totalHeight];
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < totalHeight; y++)
+                {
+                    output[x, y] = ' ';
+                }
+            }
+
+            var rootIndentation = HeightIndentation(Root.Height);
+            WriteNodeToString(Root, Root.Height, 0, rootIndentation, output, false);
+
+            return AsSingleString(output);
+        }
+
+        private string AsSingleString(char[,] output)
+        {
+            var builder = new StringBuilder();
+
+            for (var row = 0; row < output.GetLength(1); row++)
+            {
+                for (var column = 0; column < output.GetLength(0); column++)
+                {
+                    builder.Append(output[column, row]);
+                }
+
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+
+        private void WriteNodeToString(Node node, int height, int row, int column, char[,] output, bool left)
+        {
+            var valueAsString = node.Value.ToString();
+
+            if (valueAsString.Length > 1 && !left)
+            {
+                for (var x = 0; x < valueAsString.Length; x++)
+                {
+                    output[column + x - 1, row] = valueAsString[x];
+                }
+            }
+            else
+            {
+                for (var x = 0; x < valueAsString.Length; x++)
+                {
+                    output[column + x, row] = valueAsString[x];
+                }
+            }
+
+            if ((node.LeftChild ?? node.RightChild) == null)
+            {
+                return;
+            }
+
+            var indentation = HeightIndentation(node.Height);
+            var intentationDelta = indentation - HeightIndentation(node.Height - 1);
+
+            output[column, row + 1] = '┴';
+
+            if (node.LeftChild != null)
+            {
+                for (var x = 1; x < intentationDelta; x++)
+                {
+                    output[column - x, row + 1] = '-';
+                }
+
+                output[column - intentationDelta, row + 1] = '┌';
+
+                WriteNodeToString(node.LeftChild, height - 1, row + 2, column - intentationDelta, output, true);
+            }
+
+            if (node.RightChild != null)
+            {
+                for (var x = 1; x < intentationDelta; x++)
+                {
+                    output[column + x, row + 1] = '-';
+                }
+
+                output[column + intentationDelta, row + 1] = '┐';
+
+                WriteNodeToString(node.RightChild, height - 1, row + 2, column + intentationDelta, output, false);
+            }
+        }
+
+        private static int HeightIndentation(int height)
+        {
+            if (height <= 0) // Needed to figure out indent of items below it (if any)
+            {
+                return 0;
+            }
+            else if (height == 1)
+            {
+                return 0;
+            }
+            else if (height == 2)
+            {
+                return 2;
+            }
+            else
+            {
+                return 2 * HeightIndentation(height - 1) + 1;
+            }
+        }
+
         public class Node
         {
-            public int Value { get; private set; }
+            public int Value { get; }
+
+            public Node? Parent { get; private set; }
 
             public Node? LeftChild { get; set; }
 
-            public Node? RightChild { get; set; }
+            public Node? RightChild { get; set; } 
 
             public int Height { get; }
         }
